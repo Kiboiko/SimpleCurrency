bd2aa2d925c10a9443ec023934c254943130e25a
Kirill <kirboy2013@gmail.com>
2026-03-17 18:30:55 +0300

    begin

---
+++ spec/SimpleCurrency_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SimpleCurrency do
+  it "has a version number" do
+    expect(SimpleCurrency::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
